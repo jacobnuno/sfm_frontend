1c411cf352e69ce57e0b08c178fac87fc64d9891
Jacob Nuno <jacobcnda@gmail.com>
2019-04-29 19:04:25 -0500

    complex module added to vuex

---
+++ src/types/complex.js
@@ -0,0 +1,17 @@
+import namespace from '@/utils/namespace';
+
+export default namespace('complex', {
+    getters: [
+
+    ],
+    actions: [
+        'createComplex',
+        'getComplexes',
+        'getComplex',
+        'updateComplex',
+        'deleteComplex'
+    ],
+    mutations: [
+        
+    ]
+});

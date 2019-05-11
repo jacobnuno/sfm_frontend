f05d15f6420909eee08c03dda1e7db4ed512300b
Jacob Nuno <jacobcnda@gmail.com>
2019-05-11 14:30:56 -0500

    team module actions and definitions

---
+++ src/types/team.js
@@ -0,0 +1,17 @@
+import namespace from '@/utils/namespace';
+
+export default namespace('team', {
+    getters: [
+
+    ],
+    actions: [
+        'createTeam',
+        'getTeams',
+        'getTeam',
+        'updateTeam',
+        'deleteTeam'
+    ],
+    mutations: [
+
+    ]
+});
